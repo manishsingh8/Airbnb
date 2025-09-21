313157f397f7c72d7c8c80b842f458c0c4355a69
Manish Singh <msingh@dotsoft.in>
2025-09-21 21:36:43 +0530

    added-first-middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log(req.url,req.method);
+   res.send('<h1>Welcome to airbnb</h1>');
+})
+
 
 const PORT  = 3000;
 app.listen(PORT,()=>{
